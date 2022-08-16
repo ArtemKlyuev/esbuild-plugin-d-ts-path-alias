c9ecdefefb670edd796e0133ede901875c32593c
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-08-16 15:02:41 +0300

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
+    quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
     'import/extensions': [
       'error',
