5461ddbcc6fd16d64b465c78a06e1beeff5d3275
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-08-17 17:15:15 +0300

    chore: add configuration for dry-run

--- release.config.js
+++ release.config.js
@@ -1,12 +1,23 @@
+const isDryRun = process.env.NODE_ENV !== 'production';
+
+if (isDryRun) {
+  // eslint-disable-next-line global-require
+  const path = require('path');
+  // eslint-disable-next-line global-require
+  require('dotenv').config({ path: path.resolve(process.cwd(), '.env.local') });
+  console.log('Running release in dry-run mode!');
+}
+
 module.exports = {
-  branches: ['master', { name: 'beta', prerelease: true }],
-  ci: true,
+  branches: ['main', { name: 'beta', prerelease: true }],
+  ci: !isDryRun,
   debug: true,
+  dryRun: isDryRun,
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
-    '@semantic-release/npm',
+    ['@semantic-release/npm', { tarballDir: 'dist' }],
     [
       '@semantic-release/git',
       {
@@ -14,6 +25,6 @@ module.exports = {
         assets: ['package.json', 'CHANGELOG.md'],
       },
     ],
-    '@semantic-release/github',
+    ['@semantic-release/github', { assets: [{ path: 'dist/*.tgz' }] }],
   ],
 };
